b2a29baad32980a2b2e5dabd9627509167e1958f
Brendan Thompson <me@brendanthompson.com>
2019-04-08 14:21:28 +1000

    updated schema, options for provider
    - Ensure schema looks the same as for `resource_mongodbatlas_container`
    - Added options to allow for retrieval of `ProviderName` specific items

--- mongodbatlas/data_source_mongodbatlas_container.go
+++ mongodbatlas/data_source_mongodbatlas_container.go
@@ -16,29 +16,49 @@ func dataSourceContainer() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"identifier": &schema.Schema{
+			"atlas_cidr_block": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"atlas_cidr_block": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			},
 			"provider_name": &schema.Schema{
 				Type:     schema.TypeString,
-				Computed: true,
+				Required: true,
+				ForceNew: true,
 			},
 			"region": &schema.Schema{
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
+				ForceNew: true,
 			},
 			"vpc_id": &schema.Schema{
 				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"gcp_project_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"network_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"identifier": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
 				Computed: true,
 			},
 			"provisioned": &schema.Schema{
 				Type:     schema.TypeBool,
+				Optional: true,
 				Computed: true,
+			},
+			"private_ip_mode": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
 			},
 		},
 	}
@@ -57,10 +77,17 @@ func dataSourceContainerRead(d *schema.ResourceData, meta interface{}) error {
 	d.SetId(c.ID)
 	d.Set("atlas_cidr_block", c.AtlasCidrBlock)
 	d.Set("provider_name", c.ProviderName)
-	d.Set("region", c.RegionName)
-	d.Set("vpc_id", c.VpcID)
 	d.Set("identifier", c.ID)
 	d.Set("provisioned", c.Provisioned)
+
+	if d.Get("provider_name").(string) == "AWS" {
+		d.Set("region", c.RegionName)
+		d.Set("vpc_id", c.VpcID)
+	}
+	if d.Get("provider_name").(string) == "GCP" {
+		d.Set("gcp_project_id", c.GcpProjectID)
+		d.Set("network_name", c.NetworkName)
+	}
 
 	return nil
 }
